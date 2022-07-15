7a5bab1d31f5adc67f56d540b23b028b3c4769b5
= <=>
2022-07-15 12:02:45 +0530

    Initial Commit

---
+++ WeSplit-Proj1/WeSplit_Proj1App.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplit_Proj1App.swift
+//  WeSplit-Proj1
+//
+//  Created by Admin on 15/07/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplit_Proj1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
